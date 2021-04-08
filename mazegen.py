ab1a9c6732ba36ef0f3e93efeff645c2a2775b9e
Ziro <petrop@purdue.edu>
2021-04-08 17:14:10 -0400

    now has a working pathing function!!

--- mazegen.py
+++ mazegen.py
@@ -37,20 +37,17 @@ def gendir(): #pretty sure this is useless
 
 def cangen(map, xcord, ycord): #see if a new point can be generated as part of the path
     #doesn't actually matter if you mix up x and y cords, it will work regardless.
-    xtouch = 0
-    ytouch = 0
-    if map[ycord][xcord] == [0]:
-        return 0
+    touches = 0
     if map[ycord][xcord+1] == '0':
-        xtouch +=1
+        touches+=1
     if map[ycord][xcord-1] == '0':
-        xtouch +=1
+        touches+=1
     if map[ycord+1][xcord] == '0':
-        ytouch+=1
+        touches+=1
     if map[ycord-1][xcord] == '0':
-        ytouch+=1
+        touches+=1
 
-    if (ytouch <=1): #if only touching from one side (where it entered from)
+    if touches <=1: #if only touching from one side (where it entered from)
         return 1
     return 0        #all else
 
@@ -73,10 +70,50 @@ def genmainpath(map):
 
     print(cangen(map,2,3))
 
+def getlen(map): #returns how tall the map is. Note this INCLUDES the borders,
+    length = 0
+    for i in range(len(map)):
+        #print(i)
+        length+=1
+    return length
 
+def getstart(map): #finds out where to start on the map
+    for y in range(getlen(map)):
+        temp = map[y]
+        for x in range(len(temp)):
+            if map[y][x] == '?':
+                print("Start found at",y,x)
+                print(map[y][x])
+                return[y,x]
+
+def travel(map, ycord, xcord):
+    print("CURR:",map[ycord][xcord])
+    map[ycord][xcord] = 'o' #set the current coordinate to o, similiar to setting a traversed node to grey in a tree
+
+    #orientations, pretty much useless for anything other than reference
+    right = map[ycord][xcord+1]
+    left = map[ycord][xcord-1]
+    up = map[ycord-1][xcord]
+    down = map[ycord+1][xcord]
+    if up == '0':
+        #print("up")
+        return travel(map, ycord - 1, xcord)
+    if down == '0':
+        #print("down")
+        return travel(map, ycord + 1, xcord)
+    if left == '0':
+        #print("L")
+        return travel(map, ycord, xcord - 1)
+    if right == '0':
+        #print("R")
+        return travel(map, ycord, xcord + 1)
+    return [ycord,xcord]
 
 if __name__ == '__main__':
     dim = 10
     border = createborder(20,12)
     printborder(border)
+    newline()
     genmainpath(border)
+    yx = getstart(border)
+    print(travel(border,yx[0],yx[1]))
